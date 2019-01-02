b079bd91518a5df97762aee5419f27e6f2caee70
grant smith <grantsmith1256@gmail.com>
2019-01-02 19:13:10 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 require_relative '../config/environment'
+=======
+
+>>>>>>> 4df540d61517c4f6d52fe445b04e79222e39bea4
 
 Hashie
 Sinatra
--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,12 @@
+<<<<<<< HEAD
 require 'bundler/setup'
 Bundler.require(:default, :development)
+=======
+require "bundler/setup"
+require_relative "../bin/run.rb"
+
+
+if ENV['RACK_ENV'] == 'development'
+  Bundler.require(:default, :development)
+end
+>>>>>>> 4df540d61517c4f6d52fe445b04e79222e39bea4
